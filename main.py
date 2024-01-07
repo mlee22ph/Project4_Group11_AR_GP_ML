b2e1ceb2de293322b917fa3327d66cab90fd9e50
mlee22ph <137503643+mlee22ph@users.noreply.github.com>
2024-01-07 21:47:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 # Import our dependencies
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+
+st.write("Hello ,let's learn how to build a streamlit app together")
+
+tabular_data = pd.read_csv('https://mlee22ph.github.io/Project4_Group11_AR_GP_ML/Resources/HAM10000_metadata.csv')
+st.write(tabular_data.head())
